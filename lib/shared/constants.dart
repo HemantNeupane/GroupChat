60c9f54f1d23976fef154b072d2733d5bfc128ad
HemantNeupane <hemant.neupane.56@gmail.com>
2022-10-07 20:24:05 +0530

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:532650265750:web:85977c1fec37fb445c86b7";
+  static String apiKeyy = "AIzaSyCDUxbMstLK_yGKI2zJ95Eky6uQkLI8B-s";
+  static String messagingSenderId = "532650265750";
+  static String projectId = "chatappflutter-513d6";
+  final primaryColor = Color(0xFFee7b64);
+}
